8d4f8802e8c8f170012e8d42fc82a33d3e35af02
gonza <g.pacheco.2022@alumnos.urjc.es>
2025-11-14 17:33:17 +0100

    ye

--- myls.c
+++ myls.c
@@ -21,7 +21,7 @@ myls(char *dirname) {
         //printf("%s\n", entry ->d_name);
 
         //comprobamos el tipo de archivo
-        if (stat(entry->d_ name,&st)) { 
+        if (stat(entry->d_name,&st)) { 
 			err(EXIT_FAILURE,"error stat");
         }
         
