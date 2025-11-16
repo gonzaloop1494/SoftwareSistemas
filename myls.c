df6586d35d7ea954f6e61605612f5e0519ffa314
gonza <g.pacheco.2022@alumnos.urjc.es>
2025-11-16 19:00:11 +0100

    lstat

--- myls.c
+++ myls.c
@@ -3,14 +3,14 @@
 #include <err.h>
 #include <dirent.h>
 #include <sys/stat.h>
-
+#include <string.h>
 
 void
 myls(char *dirname) {
     DIR *d;
     struct dirent *entry;
     struct stat st;
-    char t; // Esta variable almacena el tipo de archivo
+    char t; // tipo de archivo
 
     d = opendir(dirname);
     if (d == NULL) {
@@ -18,33 +18,36 @@ myls(char *dirname) {
     }
 
     while((entry = readdir(d)) != NULL ) {
-        //printf("%s\n", entry ->d_name);
 
-        //comprobamos el tipo de archivo
-        if (stat(entry->d_name,&st)){ 
-			err(EXIT_FAILURE,"error stat");
+        // lstat para NO seguir enlaces simbólicos
+        if (lstat(entry->d_name, &st) < 0) {
+            warn("lstat failed on %s", entry->d_name);
+            continue;
         }
-        
 
         // Determinar el tipo de archivo
-		if (S_ISDIR(st.st_mode)) {
-			t = 'd'; //directorio
+        if (S_ISDIR(st.st_mode)) {
+            t = 'd'; // directorio
         }
-		else if (S_ISREG(st.st_mode)) {
-			t = 'f'; //archivo regular
+        else if (S_ISREG(st.st_mode)) {
+            t = 'f'; // fichero regular
         }
-		else if (S_ISLNK(st.st_mode)) {
-			t = 'l'; //enlace simbólico
+        else if (S_ISLNK(st.st_mode)) {
+            t = 'l'; // enlace simbólico
         }
-		else
-			t = 'o'; // Otro tipo de archivo
+        else
+            t = 'o'; // otro tipo
 
-        // Imprimir información del archivo
-        printf("%c	%d	%d	%lld	%s\n", t, st.st_uid,st.st_gid, st.st_size, entry->d_name);
-		
+        // Imprimir toda la información pedida: tipo, uid, gid, tamaño, nombre
+        printf("%c\t%u\t%u\t%lld\t%s\n",
+               t,
+               st.st_uid,
+               st.st_gid,
+               (long long) st.st_size,
+               entry->d_name);
     }
-		
-	closedir(d);
+
+    closedir(d);
 }
 
 int 
@@ -53,6 +56,7 @@ main(int argc, char *argv[]) {
     if (argc != 1) {
         errx(EXIT_FAILURE, "Usage: %s", argv[0]);
     }
+
     myls(".");
 
     exit(EXIT_SUCCESS);
