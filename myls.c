eb9a06ade0a394d98c4ca24e7dfa23fbbd48e5a0
gonza <g.pacheco.2022@alumnos.urjc.es>
2025-11-16 18:04:57 +0100

    porf

--- myls.c
+++ myls.c
@@ -40,7 +40,7 @@ myls(char *dirname) {
 			t = 'o'; // Otro tipo de archivo
 
         // Imprimir información del archivo
-        printf("%c\t%ld\t%s\n", t, st.st_size, st.st_uid,st.st_gid, entry->d_name);
+        printf("%c	%d	%d	%lld	%s\n", t, st.st_uid,st.st_gid, st.st_size, entry->d_name);
 		
     }
 		
