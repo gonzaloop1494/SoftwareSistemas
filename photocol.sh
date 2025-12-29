a5d99bc91a5a6792b5d258e198b0d4e2f1e74857
gonza <g.pacheco.2022@alumnos.urjc.es>
2025-12-29 12:52:32 +0100

    cambio en photocol

--- photocol.sh
+++ photocol.sh
@@ -107,7 +107,7 @@ cd "$COLLECTION" || exit 1
 #    Importante: awk '{print $9, $5}' asume columna 9=nombre, 5=tamaño.
 #    Lo mandamos a un temporal PRIMERO.
 
-# Modificación: Añadimos | grep -v "metadata"
+# Modificación: Añadimos | grep -v "metadata" este es el cambio
 ls -l | grep -v "^total" | grep -v "metadata" | awk '{print $9, $5}' | sort -k2 -n > metadata.tmp
 
 # Calculamos el total y formateamos la salida final
